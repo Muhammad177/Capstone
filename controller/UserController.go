4f0a7f3ac9934d6d1d6dc9519099d27e011cef38
Muhammad177 <wahyuyudiansyah20@gmail.com>
2023-05-24 14:55:25 +0700

    kurang Upload Photo

--- controller/UserController.go
+++ controller/UserController.go
@@ -3,6 +3,8 @@ package controller
 import (
 	"Capstone/database"
 	"Capstone/midleware"
+	"io"
+	"os"
 
 	"Capstone/models"
 	"net/http"
@@ -15,6 +17,30 @@ func CreateUserController(c echo.Context) error {
 	user := models.User{}
 	c.Bind(&user)
 	user.Role = "User"
+
+	file, err := c.FormFile("file")
+	if err != nil {
+		return echo.NewHTTPError(http.StatusBadRequest, "Failed to upload file")
+	}
+
+	// Source
+	src, err := file.Open()
+	if err != nil {
+		return echo.NewHTTPError(http.StatusInternalServerError, "Failed to open file")
+	}
+	defer src.Close()
+
+	// Destination
+	dst, err := os.Create(file.Filename)
+	if err != nil {
+		return echo.NewHTTPError(http.StatusInternalServerError, "Failed to create file")
+	}
+	defer dst.Close()
+
+	// Copy
+	if _, err = io.Copy(dst, src); err != nil {
+		return echo.NewHTTPError(http.StatusInternalServerError, "Failed to save file")
+	}
 
 	if err := database.DB.Save(&user).Error; err != nil {
 		return echo.NewHTTPError(http.StatusBadRequest, err.Error())
