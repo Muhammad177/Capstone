9705551bcc004f280389d6a578240b51d7eb5a48
Muhammad177 <wahyuyudiansyah20@gmail.com>
2023-06-03 18:37:48 +0700

    route

--- routes/route.go
+++ routes/route.go
@@ -14,5 +14,15 @@ func New() *echo.Echo {
 	// routing with query parameter
 	e.POST("/login", controller.LoginController)
 	e.POST("/user", controller.CreateUserController)
+	eJwt := e.Group("/jwt")
+	eJwt.PUT("/admin/:id", controller.UpdateUserAdminController)
+	eJwt.DELETE("/admin/:id", controller.DeleteUserAdminController)
+	eJwt.GET("/admin", controller.GetUsersAdminController)
+	eJwt.GET("/admin/:id", controller.GetUserByidAdminController)
+	eJwt.PUT("/user/:id", controller.UpdateUserController)
+	eJwt.DELETE("/user/:id", controller.DeleteUserController)
+	eJwt.GET("/user", controller.GetUserController)
+	//confirm
 	e.Logger.Fatal(e.Start(":8000"))
 	return e
+}
