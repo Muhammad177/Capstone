38245f651952db0634fa2026f19302aa44942669
sayyidpane9 <sayyidpane9@gmail.com>
2023-06-05 18:33:05 +0700

    add revisi

--- controller/UserController.go
+++ controller/UserController.go
@@ -2,6 +2,11 @@ package controller
 
 import (
 	"Capstone/database"
+	"Capstone/midleware"
+	"io/ioutil"
+	"mime"
+	"os"
+	"path/filepath"
 
 	"Capstone/models"
 	"net/http"
@@ -67,4 +72,72 @@ func DeleteUserController(c echo.Context) error {
 		"message": "success delete User by id",
 		"Produk":  users,
 	})
+}
+
+func LoginController(c echo.Context) error {
+	user := models.User{}
+	c.Bind(&user)
+	if err := database.DB.Where("email = ? AND password = ?", user.Email, user.Password).First(&user).Error; err != nil {
+		return echo.NewHTTPError(http.StatusBadRequest, map[string]interface{}{
+			"message": "Failed Login",
+			"error":   err.Error(),
+		})
+	}
+
+	token, err := midleware.CreateToken(int(user.ID), user.Username, user.Role)
+
+	if err != nil {
+		return c.JSON(http.StatusInternalServerError, map[string]interface{}{
+			"message": "Failed Login",
+			"error":   err.Error(),
+		})
+	}
+	usersResponse := models.UserResponse{int(user.ID), user.Username, user.Email, token}
+
+	return c.JSON(http.StatusOK, map[string]interface{}{
+		"message": "success Login",
+		"user":    usersResponse,
+	})
+
+}
+
+func GetImageHandler(c echo.Context) error {
+	
+	// Dapatkan UUID gambar dari parameter permintaan
+	uuid := c.Param("uuid")
+
+	// Construct the file path based on the UUID string
+	filePath := filepath.Join("uploads", uuid) // Folder "uploads" berada dalam direktori saat ini
+
+	// Dapatkan tipe MIME file
+	file, err := os.Open(filePath)
+	if err != nil {
+		return c.String(http.StatusNotFound, "File not found")
+	}
+	defer file.Close()
+
+	// Baca awal file untuk mendapatkan tipe MIME
+	buffer := make([]byte, 512) // Membaca 512 byte pertama
+	_, err = file.Read(buffer)
+	if err != nil {
+		return c.String(http.StatusInternalServerError, "Internal server error")
+	}
+
+	// Deteksi tipe MIME
+	mimeType := mime.TypeByExtension(filepath.Ext(filePath))
+	if mimeType == "" {
+		mimeType = http.DetectContentType(buffer)
+	}
+
+	// Set header Content-Type pada response
+	c.Response().Header().Set("Content-Type", mimeType)
+
+	// Baca file dengan ekstensi yang tepat
+	imageBytes, err := ioutil.ReadFile(filePath)
+	if err != nil {
+		return c.String(http.StatusInternalServerError, "Internal server error")
+	}
+
+	// Kirim data gambar sebagai response
+	return c.Blob(http.StatusOK, mimeType, imageBytes)
 }
--- models/user.go
+++ models/user.go
@@ -12,10 +12,18 @@ type User struct {
 	Photo    string `json:"photo" form:"photo"`
 	Bio      string `json:"bio" form:"bio"`
 	Role     string `json:"role" form:"role"`
+	Threads  []Thread `json:"threads"`
 }
 type UserResponse struct {
 	ID    int    `json:"id" form:"name"`
 	Name  string `json:"name" form:"name"`
 	Email string `json:"email" form:"email"`
 	Token string `json:"token" form:"token"`
+}
+
+type AdminResponse struct {
+	ID       int    `json:"id" form:"name"`
+	Name     string `json:"name" form:"name"`
+	Email    string `json:"email" form:"email"`
+	Password string `json:"password" form:"password"`
 }
