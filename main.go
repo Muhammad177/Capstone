760503f1eb5c5e499d9459749102f7d530a13757
Muhammad177 <wahyuyudiansyah20@gmail.com>
2023-06-26 05:52:37 +0700

    Deploy

--- main.go
+++ main.go
@@ -4,23 +4,14 @@ import (
 	"Capstone/database"
 	"Capstone/routes"
 	"fmt"
-	"log"
 	"os"
 
-	"github.com/joho/godotenv"
 	"github.com/labstack/echo/v4"
 )
 
 const DEFAULT_PORT = "8080"
 
 func main() {
-	if env := os.Getenv("ENV"); env != "production" {
-		log.Println(env)
-		err := godotenv.Load(".env")
-		if err != nil {
-			log.Fatal("failed to load .env")
-		}
-	}
 	database.Init()
 
 	app := echo.New()
